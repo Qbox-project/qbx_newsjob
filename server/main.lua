359a6816f877dda35a02eca2122b4a7a6b2f69e7
Snipe <10562121+pushkart2@users.noreply.github.com>
2026-07-15 21:22:50 -0500

    fix: secure news vehicle spawning

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,9 @@
+local config = require 'config.client'
+local vehiclesSpawning = {}
+
 local function checkReporterJob(source)
-    local Player = exports.qbx_core:GetPlayer(source)
-    return Player.PlayerData.job.name == 'reporter'
+    local player = exports.qbx_core:GetPlayer(source)
+    return player and player.PlayerData.job.name == 'reporter' and player
 end
 
 lib.addCommand('newscam', {
@@ -33,12 +36,34 @@ lib.addCommand('newsbmic', {
     TriggerClientEvent('qbx_newsjob:client:toggleBMic', source)
 end)
 
-lib.callback.register('qbx_newsjob:server:spawnVehicle', function(source, model, coords, plate)
-    local netId, veh = qbx.spawnVehicle({ model = model, spawnSource = coords, props = { plate = plate } })
+lib.callback.register('qbx_newsjob:server:spawnVehicle', function(source, model)
+    local player = checkReporterJob(source)
+    if not player or vehiclesSpawning[source] or type(model) ~= 'string' then return end
+
+    local grade = player.PlayerData.job.grade.level
+    local spawnCoords
+    if config.authorizedVehicles[grade]?[model] then
+        spawnCoords = config.locations.vehicleStorage.coords
+    elseif config.authorizedhelicopters[grade]?[model] then
+        spawnCoords = config.locations.helicopterStorage.coords
+    else
+        return
+    end
+
+    if #(GetEntityCoords(GetPlayerPed(source)) - spawnCoords.xyz) > 10.0 then return end
+
+    vehiclesSpawning[source] = true
+    local plate = ('NEWS%s'):format(lib.string.random('1111'))
+    local netId, veh = qbx.spawnVehicle({ model = model, spawnSource = spawnCoords, props = { plate = plate } })
+    vehiclesSpawning[source] = nil
     if not netId or netId == 0 then return end
     if not veh or veh == 0 then return end
-    SetEntityHeading(veh, coords.w)
+    SetEntityHeading(veh, spawnCoords.w)
     SetVehicleNumberPlateText(veh, plate)
     TriggerClientEvent('vehiclekeys:client:SetOwner', source, plate)
     return netId
+end)
+
+AddEventHandler('playerDropped', function()
+    vehiclesSpawning[source] = nil
 end)
